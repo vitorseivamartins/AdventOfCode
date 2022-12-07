db44d6f1a3ec5ec26dfca0e2afdba29b680a1689
Vitor Martins <vitorseivamartins@gmail.com>
2022-12-07 19:58:51 -0300

    Day01 - 2022

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("PartOne: " + Solution.PartOne());
+Console.WriteLine("PartTwo: " + Solution.PartTwo());
+
+static class Solution
+{
+    public static int PartOne()
+    {
+        var inputData = File.ReadAllText("..//..//..//input.txt");
+
+        var inputByElf = inputData.Split("\r\n\r\n").ToList();
+        var totalCaloriesFoundInTop1Elf = 0;
+        foreach(var elf in inputByElf)
+        {
+            var totalCaloriesOfCurrentElf = elf.Split("\r\n").Sum(x => int.Parse(x));
+            if (totalCaloriesOfCurrentElf > totalCaloriesFoundInTop1Elf)
+                totalCaloriesFoundInTop1Elf = elf.Split("\r\n").Sum(x => int.Parse(x));
+        }
+
+        return totalCaloriesFoundInTop1Elf;
+    }
+
+    public static int PartTwo()
+    {
+        var inputData = File.ReadAllText("..//..//..//input.txt");
+
+        var totalByElf = new List<int>();
+        foreach (var elf in inputData.Split("\r\n\r\n"))
+            totalByElf.Add(elf.Split("\r\n").Sum(x => int.Parse(x)));
+
+        return totalByElf.OrderByDescending(x => x).Take(3).Sum();
+    }
+}
